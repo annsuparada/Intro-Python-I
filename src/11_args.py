da70f4ec5129e2282b2a45f42a47339b8e601578
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-03 17:28:15 -0600

    args

--- src/11_args.py
+++ src/11_args.py
@@ -5,13 +5,18 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+print("===f1===")
+def f1(num1, num2):
+    return num1 + num2
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+print("===f2===")
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,38 +26,51 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22 # if take from a list, have to add * 
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+print("===f3===")
+def f3(a=1,b=1):
+    return a + b
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
-#
+    
 # key: foo, value: bar
 # key: baz, value: 12
 #
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+print("===f4===")
+def f4(a=1, b=2):
+    print("key: a, value: ", a)
+    print("key: b, value: ", b)
+    return
+
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
 
+def f5(city="..", population=2, founded=".."):
+    print("key: city, value: ", city)
+    print("key: population, value: ", population)
+    print("key: founded, value: ", founded)
+  
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f5(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
@@ -60,4 +78,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f5(dict(**d))
